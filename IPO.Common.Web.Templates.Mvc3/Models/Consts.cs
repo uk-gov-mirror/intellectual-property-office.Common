0d4f77bffa093bd5443573d4e415e75796bbd11c
IPO-david-debenedictis <david.debenedictis@ipo.gov.uk>
2014-05-15 11:11:32 +0100

    Initial commit of the Gov UK templates.

---
+++ IPO.Common.Web.Templates.Mvc3/Models/Consts.cs
@@ -0,0 +1,10 @@
+﻿namespace IPO.Common.Web.Templates.Mvc3.Models
+{
+    public static class Consts
+    {
+        public const string FeedbackMailToUrl = "mailto:feedback@patent.service.gov.uk?subject=Patent renewal feedback";
+        public const string HelpUrlBeta = "https://www.gov.uk/help/beta";
+        public const string HelpUrlAlpha = "https://www.gov.uk/service-manual/phases/alpha";
+    }
+   
+}
---
+++ IPO.Common.Web.Templates.Mvc3/Models/Enums.cs
@@ -0,0 +1,16 @@
+﻿namespace IPO.Common.Web.Templates.Mvc3.Models
+{
+    public enum FeedbackMethod
+    {
+        None = 0,
+        MailTo = 1,
+        Form = 2
+    }
+
+    public enum ServiceWarningType
+    {
+        None = 0,
+        Alpha = 1,
+        Beta = 2
+    }
+}
